9c01349ae605d806629d3c73f9af8d1bd4d6c509
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-03 18:07:43 +0530

    [Vedhashni] Updated the file with to create contact and add details

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    /// <summary>
+    /// To Create Contacts
+    /// </summary>
+    class AddressBook
+    {
+        public string FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email;
+        public static List<AddressBook> Person = new List<AddressBook>();
+       
+        // Getting details of contacts
+        public static void Getdetails()
+        {
+            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Enter First Name : ");
+             addressBook.FirstName= Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            addressBook.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            addressBook.Address = Console.ReadLine();
+            Console.WriteLine("Enter City : ");
+            addressBook.City = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            addressBook.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip code : ");
+            addressBook.Zip = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number : ");
+            addressBook.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            addressBook.Email = Console.ReadLine();
+
+            Person.Add(addressBook);
+        }
+        public static void DisplayContact(AddressBook addressBook)
+        {
+            Console.WriteLine("First Name: " + addressBook.FirstName);
+            Console.WriteLine("Last Name: " + addressBook.LastName);
+            Console.WriteLine("Phone Number: " + addressBook.PhoneNumber);
+            Console.WriteLine("Address : " + addressBook.Address);
+            Console.WriteLine("City : " + addressBook.City);
+            Console.WriteLine("State : " + addressBook.State);
+            Console.WriteLine("ZipCode : " + addressBook.Zip);
+            Console.WriteLine("Phone Number: " + addressBook.PhoneNumber);
+            Console.WriteLine("EmailId: " + addressBook.Email);
+           
+        }
+        public static void AddContact()
+        {
+            if (Person.Count == 0)
+            {
+                Console.WriteLine("Your address book contacts are empty.");
+                Console.ReadKey();
+                return;
+            }
+            
+            foreach (var addressBook in Person)
+            {
+                DisplayContact(addressBook);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -2,11 +2,31 @@
 
 namespace AddressBookSystem
 {
+    /// <summary>
+    /// To create and add contact details
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System!");
+            int option = int.Parse(Console.ReadLine());
+            
+            //'1' - is used to create and add contact details
+
+            switch (option)
+            {
+                case 1:
+                    AddressBook.Getdetails();
+                    AddressBook.AddContact();
+                    break;
+
+                case 2:
+                    return;
+
+                default:
+                    break;
+            }
         }
     }
 }
